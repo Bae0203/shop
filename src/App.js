5e34fe30c45592a763bf13b6b40ff4c590137484
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-09 20:04:06 +0900

    ADD :: 숙제 부분
    map을 사용하여 컴포넌트 띄우기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
+import { useState } from "react";
+import data from "./Server";
 
 function App() {
+  let [shoes] = useState(data);
+  console.log(shoes[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main_bg"></div>
+      <Container>
+        <Row>
+          {shoes.map(function (e, i) {
+            return <Card data={e} index={i} key={i} />;
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Card = (props) => {
+  console.log("e", props.data, props.index);
+  return (
+    <Col>
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`}
+        width="80%"
+      />
+      <h4>{props.data.title}</h4>
+      <p>{props.data.price}</p>
+      <p>{props.data.content}</p>
+    </Col>
+  );
+};
