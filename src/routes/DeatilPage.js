c44d949e88701db12b970776a09ccec71670dca2
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-12 00:26:41 +0900

    FIX :: props 넘기기
    컴포넌트 추가 -> props넘겨서 상품의 id로
    상품들 띄우기

--- src/routes/DeatilPage.js
+++ src/routes/DeatilPage.js
@@ -21,15 +21,9 @@ const DeatilPage = (props) => {
           {/* 응용하는 방법 : 상품 정렬 버튼이 있다 하면 분명 꼬일거임
           예) 0번이 하하 였는데 갑자기 0번 페이지가 가가가가 로 변함
           따라서 URL파라미터 접속시 0번째 상품말고 상품의 id로 구분하는게 좋을 듯*/}
-          <h4 className="pt-5">
-            {() => {
-              let title = props.shoes.filter((i) => i.id == id);
-              console.log("ti : ", title);
-              return <div>dd</div>;
-            }}
-          </h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className="pt-5">{Title(props, id)}</h4>
+          <p>{Content(props, id)}</p>
+          <p>{Price(props, id)}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
@@ -38,3 +32,21 @@ const DeatilPage = (props) => {
 };
 
 export default DeatilPage;
+
+const Title = (props, id) => {
+  let title = props.shoes.filter((i) => i.id == id);
+  console.log("ti : ", title[0].title);
+  return title[0].title;
+};
+
+const Content = (props, id) => {
+  let title = props.shoes.filter((i) => i.id == id);
+  console.log("ti : ", title[0].title);
+  return title[0].content;
+};
+
+const Price = (props, id) => {
+  let title = props.shoes.filter((i) => i.id == id);
+  console.log("ti : ", title[0].title);
+  return title[0].price;
+};
