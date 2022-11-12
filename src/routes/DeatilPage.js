1a38fff287b7ea67a1221702aca18c9bb257bfb0
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-12 21:40:58 +0900

    ADD :: styled-components 설명 추가
    styled-components를 간단하게 설명해주는 설명글 추가, 모르는 정보들 추가

--- src/routes/DeatilPage.js
+++ src/routes/DeatilPage.js
@@ -1,6 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import "../App.css";
+
+//얘를 쓰면 css파일과 다르데 다른 js파일을 오염안시킨다.
+//React특성상 모든 파일을 하나로 만들어주는데 그때 css도 하나로 만들어져서
+//파일에 묻히는데 그 css파일을 얘를들어 App.js의 종속파일로 만들고 싶으면
+//App.module.css 라고 작명하면 저 css파일은 App.js파일에만 적용되서 오염이 안된다
+//아 그리고 이거 쓰면 로딩시간 단축됨
+import styled from "styled-components";
+
+//styled-component에도 props문법을 쓸수 있는데
+/**
+ * 예를들어
+ * let Abtm = styled.button`
+ *  background-color : ${props=>props.bg}
+ *
+ *  간단한 프로그래밍도 가능한데
+ *  color : ${props => props.bg == 'black'? 'white':'black'}
+ *  이렇게 간단한 프로그래밍도 가능하다
+ * `
+ *
+ * <Abtn bg="red"> dd </Abtn>
+ *
+ * 이렇게 하면 props를 넘겨 받을 수 있다
+ */
+
+/**
+ * 기존에 했던 스타일을 복사할 수도 있는데
+ * let Bbtn = styled.button(Abtn)`
+ *  ...
+ * `
+ * 이렇게 커스터마이징도 할 수 있다.
+ */
 
 const DeatilPage = (props) => {
   //유저가 입력한 파라미터가 나옴
