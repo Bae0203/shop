f708b552718a0f6c265df27d3614225de248ed30
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-15 23:31:20 +0900

    ADD :: CSS Animation(Detail)
    Deatil Page CSS 애니메이션 추가

--- src/routes/DeatilPage.js
+++ src/routes/DeatilPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { Nav } from "react-bootstrap";
+import "../App.css";
 /**
  * 컴포넌트의 Life cycle
  * 1. 페이지에 장착 (mount)
@@ -13,6 +14,8 @@ const DeatilPage = (props) => {
   let [hidediv, setHidediv] = useState(true);
   let [inputval, setInputval] = useState(0);
   let [numCh, setNumCh] = useState(true);
+  let [tab, setTab] = useState(0);
+  let [fade, setFade] = useState("");
 
   //detail 컴포넌트가 mount되거나 update될때 마다 실행함
   /**
@@ -63,12 +66,21 @@ const DeatilPage = (props) => {
       console.log(numCh);
     }
   }, [inputval]);
-
   //유저가 입력한 파라미터가 나옴
   let { id } = useParams();
 
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setFade("end");
+    }, 10);
+    return () => {
+      setFade("");
+      clearTimeout(a);
+    };
+  }, [tab]);
+
   return (
-    <div className="container">
+    <div className={"container start " + fade}>
       {hidediv ? (
         <div className="alert alert-warning">2초 이내 구매시 할인</div>
       ) : null}
@@ -102,11 +114,79 @@ const DeatilPage = (props) => {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      {/* 
+          defaultActiveKey : 기본적으로 눌려져 있을 버튼 */}
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link0"
+            onClick={() => {
+              setTab(0);
+            }}
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link1"
+            onClick={() => {
+              setTab(1);
+            }}
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link2"
+            onClick={() => {
+              setTab(2);
+            }}
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* html안에서 if 조건문 쓰는 법 (캄포넌트 안에 넣기)*/}
+      <Tabcontent tab={tab} />
     </div>
   );
 };
 
 export default DeatilPage;
+
+const Tabcontent = ({ tab }) => {
+  let [fade, setFade] = useState("");
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setFade("end");
+    }, 100);
+
+    //automatic batching
+    // State를 바꾸면 바꿀때 마다 렌더링 되는게 아니라 마지막에 한꺼번에 렌더링이 된다.
+    //그래서 미세한 시간 차이를 두면 state를 바꿀때 마다 렌더링이 된다.
+    return () => {
+      setFade("");
+      clearTimeout(a);
+    };
+  }, [tab]);
+  // if (tab == 0) {
+  //   return <div>내용0</div>;
+  // } else if (tab == 1) {
+  //   return <div>내용1</div>;
+  // } else if (tab == 2) {
+  //   return <div>내용2</div>;
+  // }
+
+  return (
+    <div className={"start " + fade}>
+      {/* 앞에 있는 배열에서 tab번째에 있는걸 꺼내주세요~ 라는 뜻임 */}
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}
+    </div>
+  );
+};
 
 const IdCheck = (props, id, chnum) => {
   let title = props.shoes.filter((i) => i.id == id);
